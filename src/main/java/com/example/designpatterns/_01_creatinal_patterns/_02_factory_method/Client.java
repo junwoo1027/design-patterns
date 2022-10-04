536dacc26fff0c080f2595a6c87257a352afafc3
kimjunwoo <junwoo1027@naver.com>
2022-10-04 20:30:13 +0900

    팩토리 메소드 인터페이스 적용하기

--- src/main/java/com/example/designpatterns/_01_creatinal_patterns/_02_factory_method/Client.java
+++ src/main/java/com/example/designpatterns/_01_creatinal_patterns/_02_factory_method/Client.java
@@ -2,10 +2,18 @@ package com.example.designpatterns._01_creatinal_patterns._02_factory_method;
 
 public class Client {
     public static void main(String[] args) {
-        Ship whiteShip = new WhiteShipFactory().orderShip("whiteShip", "junwoo1027@naver.com");
-        System.out.println(whiteShip);
+//        Ship whiteShip = new WhiteShipFactory().orderShip("whiteShip", "junwoo1027@naver.com");
+//        System.out.println(whiteShip);
+//
+//        Ship blackShip = new BlackShipFactory().orderShip("blackShip", "junwoo1027@naver.com");
+//        System.out.println(blackShip);
 
-        Ship blackShip = new BlackShipFactory().orderShip("blackShip", "junwoo1027@naver.com");
-        System.out.println(blackShip);
+        Client client = new Client();
+        client.print(new WhiteShipFactory(), "whiteShip", "junwoo1027@naver.com");
+        client.print(new BlackShipFactory(), "blackShip", "junwoo1027@naver.com");
+    }
+
+    private void print(ShipFactory shipFactory, String name, String email) {
+        System.out.println(shipFactory.orderShip(name, email));
     }
 }
